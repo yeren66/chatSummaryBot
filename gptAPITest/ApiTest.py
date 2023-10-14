d1122e90a3ff21c2c5d228a329bfe27d703029da
yeren66 <794493836@qq.com>
2023-10-14 14:53:04 +0800

    gptApi接口编写

--- gptAPITest/ApiTest.py
+++ gptAPITest/ApiTest.py
@@ -8,14 +8,14 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_base = os.getenv("OPENAI_API_BASE")
 
-with open('C:\project\chatHelper\gptAPITest\chata.txt', 'r', encoding='utf-8') as file:
+with open('chata.txt', 'r', encoding='utf-8') as file:
     content = file.read()
 
 # print(content)
 
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
-  messages=[{"role": "user", "content": "请帮我总结以下的一段群聊对话，在总结中请用“邱佬”代替邱文杰，用“曹哥”代替曹永恒:\n" + content}]
+  messages=[{"role": "user", "content": "请帮我总结以下的一段群聊对话:\n" + content}]
 )
 
 print(completion.choices[0].message.content)
